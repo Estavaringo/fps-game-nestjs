00c44a4f060bd6dc2e0b0137322ef0f0d2277de1
Gabriel Estavaringo <gabriel.estavaringo@arquivei.com.br>
2025-03-15 10:22:50 -0300

    Remove network listeners and add console interface
    This application is a simple standalone application, so the network listener is being replaced by a console interface

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createApplicationContext(AppModule);
+
+  const readline = require('node:readline');
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question(`Digite alguma coisa: `, input => {
+    console.log(`Você digitou: ${input}`);
+    rl.close();
+  });
 }
 bootstrap();
